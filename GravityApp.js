6c6a2f2c4a9afd3822c46426cf1f14c923098285
davidychow87 <davidychow87@gmail.com>
2016-09-15 20:37:45 -0700

    Update GravityApp.js

--- GravityApp.js
+++ GravityApp.js
@@ -4,8 +4,8 @@
 * and open the template in the editor.
 */
 var canvas = document.getElementById('canvas');
-var width = 800;
-var height = 800;
+var width = 600;
+var height = 600;
 canvas.width = width;
 canvas.height = height;
 var ctx = canvas.getContext('2d');
